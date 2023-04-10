be8ad4c485e8d79e5fdcedf38f5a66cdbc703cb4
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2023-04-10 11:23:37 +0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function inputIPK(){
+    const content =`
+    <p>
+    <label>IPK</label>
+    <input type="text" name='company'>
+    </p>`
+    document.getElementById("write").innerHTML = content;
+    // document.getElementById("semester").value=10;
+}
