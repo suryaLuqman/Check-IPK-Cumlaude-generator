8a0c7c4f61c98356b368e9bac88c472ccd19831e
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2023-04-14 22:03:31 +0700

    fix UI

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,98 @@
 const formContainer = document.getElementById('form');
-const addButton = document.getElementById('submit');
+const addRemoveButton = document.getElementById('submit');
+const submitButton = document.getElementById('submitButton');
 
 // Fungsi untuk menghapus semua input form dan label dari paragraf kedua ke bawah
 function resetForm() {
-  const secondParagraph = formContainer.querySelector('p:nth-child(2)');
-  while (secondParagraph.nextSibling) {
-    formContainer.removeChild(secondParagraph.nextSibling);
-  }
+    const secondParagraph = formContainer.querySelector('p:nth-child(2)');
+    while (secondParagraph.nextSibling) {
+        formContainer.removeChild(secondParagraph.nextSibling);
+    }
 }
 
-function resetPicture(){
-    const rightPane=document.getElementById('rightPane');
-    rightPane.innerHTML="";
+function resetPicture() {
+    const picture = document.getElementById('picture');
+    // picture.src = 'assets/img/placeholder.png';
 }
 
 // Fungsi untuk menambahkan input form dan label
 function addForm() {
-  const semesterCount = document.getElementById('semesterCount').value;
+  const inputCount = document.getElementById('semesterCount').value;
+  const rightPane= document.getElementById('rightPane');
 
-    resetForm(); // Hapus semua input form dan label dari paragraf kedua ke bawah
-    resetPicture();
+  resetForm(); // Hapus semua input form dan label dari paragraf kedua ke bawah
+  resetPicture(); // Reset gambar
 
-    if (semesterCount <= 8){
+    if (inputCount > 8) {
+        rightPane.innerHTML = `Mau Ngapain Loe`;
         
-        for (let i = 0; i < semesterCount; i++) {
-          // Buat dua input dan dua label untuk setiap input
-          for (let j = 0; j < 2; j++) {
-            const inputLabel = document.createElement('label');
-            const labelName=['IPK Semester','Total SKS']
-            inputLabel.for = `input-${i}-${j}`;
-          //   inputLabel.textContent = `Input ${i+1} (${j+1}):`;
-            inputLabel.textContent = `${labelName[j]} ${i+1}:`;
-      
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.name = `input-${i}-${j}`;
-            input.id = `input-${i}-${j}`;
-            input.placeholder = `semester ${i+1} `;
-      
-            const inputContainer = document.createElement('p');
-            inputContainer.appendChild(inputLabel);
-            inputContainer.appendChild(input);
-      
-            formContainer.appendChild(inputContainer);
-          }
-        }
     }else{
-        // alert("Maksimal 8 Semester");
-        const rightPane=document.getElementById('rightPane');
-        rightPane.innerHTML="Loe Mau Ngapain?Jadi Donatur Tetap?";
+
+        for (let i = 0; i < inputCount; i++) {
+            // Buat dua input dan dua label untuk setiap input
+            for (let j = 0; j < 2; j++) {
+                const inputLabel = document.createElement('label');
+                const labelNames = [`IPK Semester ${i+1} `, `SKS Semester ${i+1}`];
+                inputLabel.for = `input-${i}-${j}`;
+                inputLabel.textContent = labelNames[j];
+    
+                const input = document.createElement('input');
+                const inputTypes = ['number', 'number'];
+                const inputNames = [`ipk-${i+1}`, `sks-${i+1}`];
+                const inputIds = [`ipk-${i+1}`, `sks-${i+1}`];
+                const inputMin = [0, 0];
+                const inputMax = [4, 25];
+                const inputStep = [0.01, 1];
+                const inputPlaceholder = [`Range 0-4`, `Range 0-25`];
+                input.type = inputTypes[j];
+                input.name = inputNames[j];
+                input.id = inputIds[j];
+                input.className = 'inputValue';
+                input.min = inputMin[j];
+                input.max = inputMax[j];
+                input.step = inputStep[j];
+                input.placeholder = inputPlaceholder[j];
+            input.addEventListener('input', validateForm); // Menambahkan event listener untuk validasi form
+    
+                const inputContainer = document.createElement('p');
+                inputContainer.appendChild(inputLabel);
+                inputContainer.appendChild(input);
+    
+                formContainer.appendChild(inputContainer);
+            }
+        }
     }
 
+
+  validateForm(); // Validasi form setelah menambahkan input form dan label baru
 }
 
-addButton.addEventListener('click', function() {
-  addForm();
+// Fungsi untuk memvalidasi form dan menentukan apakah tombol submit harus di-disable
+function validateForm() {
+  const inputs = formContainer.querySelectorAll('input[type=text]');
+  let allInputsFilled = true;
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === '') {
+      allInputsFilled = false;
+      break;
+    }
+  }
+
+  submitButton.disabled = !allInputsFilled; // Disable tombol submit jika ada setidaknya satu input yang belum diisi
+}
+
+addRemoveButton.addEventListener('click', function() {
+  const inputCount = document.getElementById('semesterCount').value;
+
+  if (inputCount == 0) {
+    resetForm(); // Jika inputCount adalah 0, hapus semua input form dan label
+  } else {
+    addForm(); // Jika inputCount bukan 0, tambahkan input form dan label
+  }
 });
 
 form.addEventListener('submit', function(e) {
-  e.preventDefault(); // Mencegah form dikirim ke server
-  addForm();
+    e.preventDefault();
+    alert('Form berhasil dikirim!'); // Ganti dengan kode pengiriman form ke server yang sesuai
 });
